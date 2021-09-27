88b58f56579b7f3d09bcb44d7760351d3ffdf6f2
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-09-27 18:48:19 +0300

    added colors

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,9 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+
+  // document.body.style.backgroundColor = 'red';
+ 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
